6797d9e89e4076d25c36815e6ef2f2f0b6f574f5
Rostyslav Solopatych <rsol240395@gmail.com>
2016-10-14 22:59:06 +0200

    Updated all scripts and models

--- batch/run_sqlplus.bat
+++ batch/run_sqlplus.bat
@@ -1,5 +1,5 @@
 echo off
 C:
 cd C:\oracle\product\11.2.0\dbhome_1\BIN
-sqlplus.exe -S rsol/rsol@orarsol @D:\STUDENT\WROCLAW\Semestr_1\Advanced_Databases\batch\script.ddl
+sqlplus.exe -S REST_1/1@ORARSOL @D:\STUDENT\WROCLAW\Semestr_1\Advanced_Databases\batch\create.ddl
 pause
