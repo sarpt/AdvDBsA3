4316c761eb39eaa23b97f8d0982f4256bf083457
Rostyslav Solopatych <rsol240395@gmail.com>
2016-10-19 19:32:41 +0200

    Added transactions #5 and #6

---
+++ batch/transaction_6.sql
@@ -0,0 +1,76 @@
+-- #6 Remove worst Chef:
+	/*SELECT all recipes in which chef participated
+	UPDATE RECIPE_DUTIES 
+	DELETE FROM SCHEDULE
+	DELETE FROM EMPLOYEE*/
+set serveroutput on
+variable n number
+exec :n := dbms_utility.get_time
+/
+ALTER SYSTEM FLUSH SHARED_POOL;
+/
+DECLARE        
+    chef_id NUMBER;
+BEGIN
+    -- define worst chef
+    WITH t1 AS
+    (
+        SELECT E.EMPLOYEEID, 
+               E.FIRSTNAME, 
+               E.LASTNAME,
+               P.SALARY,
+               P.TITLE,
+               COUNT(CL.RECIPEID) AS RECIPECOUNT 
+        FROM COOK_LOG CL
+        INNER JOIN RECIPE R
+        ON R.RECIPEID = CL.RECIPEID
+        INNER JOIN RECIPE_DUTY RD
+        ON RD.RECIPEID = CL.RECIPEID
+        INNER JOIN EMPLOYEE E
+        ON RD.EMPLOYEEID = E.EMPLOYEEID
+        INNER JOIN POSITION P
+        ON P.POSITIONID = E.POSITIONID
+        WHERE UPPER(P.TITLE) LIKE '%COOK%'
+        AND CL.DATESOLD BETWEEN '10/11/2016' AND '10/12/2016' 
+        GROUP BY E.EMPLOYEEID, E.FIRSTNAME, E.LASTNAME, P.SALARY, P.TITLE
+        ORDER BY COUNT(R.RECIPEID) ASC
+    )                         
+    SELECT EMPLOYEEID INTO chef_id 
+            /*LASTNAME,
+            FIRSTNAME,
+            RECIPECOUNT,
+            SALARY,
+            TITLE*/
+    FROM t1
+    WHERE RECIPECOUNT <= 
+        (
+            SELECT MIN(RECIPECOUNT)
+            FROM t1
+        )
+    AND ROWNUM = 1;
+
+    UPDATE RECIPE
+    SET STATE = 'UNAVAILABLE'
+    WHERE RECIPEID IN 
+    (
+        SELECT rec.RECIPEID
+        FROM RECIPE rec
+        INNER JOIN RECIPE_DUTY rd
+        ON rec.RECIPEID = rd.RECIPEID
+        INNER JOIN EMPLOYEE emp        
+        ON rd.EMPLOYEEID = emp.EMPLOYEEID
+        WHERE emp.EMPLOYEEID = chef_id
+    );
+    /*
+    DELETE FROM RECIPE_DUTY
+    WHERE EMPLOYEEID = chef_id;       
+    
+    DELETE FROM SCHEDULE
+    WHERE EMPLOYEEID = chef_id;
+    
+    DELETE FROM EMPLOYEE
+    WHERE EMPLOYEEID = chef_id;
+    */     
+END;
+exec :n := (dbms_utility.get_time - :n)/100
+exec dbms_output.put_line('Execution time '||:n||' sec')
