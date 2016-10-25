65ecc9aff096e3b966b0a2a2e3b2c8db7053b6c6
Rostyslav Solopatych <rsol240395@gmail.com>
2016-10-25 19:46:17 +0200

    Added new data & conunt_all script

---
+++ batch/all_recs_count.sql
@@ -0,0 +1,25 @@
+SELECT COUNT(*) FROM position;
+/
+SELECT COUNT(*) FROM employee;
+/
+SELECT COUNT(*) FROM schedule;
+/
+SELECT COUNT(*) FROM recipe;
+/
+SELECT COUNT(*) FROM recipe_duty;
+/
+SELECT COUNT(*) FROM ingredient_stock;
+/
+SELECT COUNT(*) FROM supplier;
+/
+SELECT COUNT(*) FROM supplier_stock;
+/
+SELECT COUNT(*) FROM supply_request;
+/
+SELECT COUNT(*) FROM ingredient;
+/
+SELECT COUNT(*) FROM resources;
+/
+SELECT COUNT(*) FROM cook_log;
+/
+
