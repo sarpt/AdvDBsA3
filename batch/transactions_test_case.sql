5e2833921a1a68827c4c68c67e71e69287bd2a08
Rostyslav Solopatych <rsol240395@gmail.com>
2016-10-26 18:49:27 +0200

    Added test case

---
+++ batch/transactions_test_case.sql
@@ -0,0 +1,36 @@
+-- trans 1,3,4
+update supplier_stock
+set weightavail = weightavail * 1000;
+
+select * 
+from ingredient_stock t1 
+inner join ingredient t2
+on t1.INGRSTOCKID = t2.INGRSTOCKID
+inner join recipe t3
+on t2.RECIPEID = t3.RECIPEID
+where t3.recipeid = 297
+
+select *
+from supply_request
+where ingrstockid = 22615
+
+select *
+from supplier_stock t1
+inner join supplier t2
+on t1.SUPPLIERID = t2.SUPPLIERID
+where ingrstockid = 22615
+
+select *
+from resources
+order by resourceid desc
+
+select *
+from cook_log
+order by cooklogid desc
+
+-- trans 5,6
+insert into resources(RESOURCEID,DATERECEIVED,TOTAL,TYPE)
+values (999999, (select current_date from dual), 10000000000, 'INVESTMENT');
+
+select * from employee
+where lastname = 'Kowalski'
