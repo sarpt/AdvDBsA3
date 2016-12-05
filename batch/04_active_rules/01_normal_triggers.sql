6ad5711062dc68f6263e8e0e997691ff3de892f6
sarpt <sarpt.contact@gmail.com>
2016-12-05 18:12:38 +0100

    Added active rule #5

--- batch/04_active_rules/01_normal_triggers.sql
+++ batch/04_active_rules/01_normal_triggers.sql
@@ -29,17 +29,42 @@ END;
 
 -- active rule #5
 CREATE OR REPLACE TRIGGER supply_request_exec
-AFTER INSERT OR UPDATE ON SUPPLY_REQUEST
+BEFORE INSERT OR UPDATE ON SUPPLY_REQUEST
 FOR EACH ROW
 DECLARE
   is_present number;
   weight_missing number;
-  nid number;
   total_price number;
   total_outcome number;
   total_res number;
+  supplier_price number;
 BEGIN
+  select count(*) INTO is_present FROM INGREDIENT_STOCK WHERE ingrstockid=:new.ingrstockid;
+  SELECT WEIGHTMISSING INTO weight_missing FROM INGREDIENT_STOCK WHERE ingrstockid=:new.ingrstockid;
 
+  IF (is_present > 0) AND (weight_missing > 0) AND (:new.STATE = 'UNSATISFIED') THEN
 
+    SELECT MIN(PRICE) INTO supplier_price FROM SUPPLIER_STOCK WHERE INGRSTOCKID=:new.INGRSTOCKID;
+                          
+    SELECT SUM(TOTAL) INTO total_outcome
+    FROM resources res
+    WHERE UPPER(res.TYPE) = 'TRANSFER';
+    
+    SELECT SUM(TOTAL) - total_outcome INTO total_res
+    FROM resources res        
+    WHERE UPPER(res.TYPE) IN ('PAYMENT', 'INVESTMENT');
+    
+    IF (weight_missing * supplier_price <= total_res) THEN
+      UPDATE INGREDIENT_STOCK
+      SET WEIGHTAVAIL = WEIGHTAVAIL + WEIGHTMISSING,
+        WEIGHTMISSING = 0.0
+      WHERE INGRSTOCKID=:new.INGRSTOCKID;
+                          
+      INSERT INTO resources VALUES ((SELECT MAX(RESOURCEID) + 1 FROM RESOURCES), SYSDATE, weight_missing * supplier_price,'TRANSFER');
+      
+      :new.STATE:='SATISFIED';           
+    END IF;
+    
+  END IF;
 END;
 /
